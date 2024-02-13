20e0d9048d50756261d820c2b667685bf2184701
Andy Sterkowitz <admin@andysterkowitz.com>
2024-02-13 13:00:23 -0500

    Creates basic entity framework setup

---
+++ src/FantasyStockTrader.Core/DatabaseContext/FantasyStockTraderContextFactory.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace FantasyStockTrader.Core.DatabaseContext
+{
+    public class FantasyStockTraderContextFactory
+    {
+        public FantasyStockTraderContext CreateDbContext()
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<FantasyStockTraderContext>();
+            optionsBuilder.UseNpgsql(x => x.MigrationsAssembly("FantasyStockTrader.Core"));
+            optionsBuilder.EnableSensitiveDataLogging();
+
+            return new FantasyStockTraderContext(optionsBuilder.Options);
+        }
+    }
+}
