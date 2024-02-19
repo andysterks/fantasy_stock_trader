6d88e085c8bb59308729a7fbc5284cb67f04a3dc
Andy Sterkowitz <admin@andysterkowitz.com>
2024-02-19 01:39:40 -0500

    Working on implementing JWT's

--- src/FantasyStockTrader/Controllers/AuthController.cs
+++ src/FantasyStockTrader/Controllers/AuthController.cs
@@ -1,4 +1,8 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
 
 namespace FantasyStockTrader.Web.Controllers;
 
@@ -6,6 +10,13 @@ namespace FantasyStockTrader.Web.Controllers;
 [ApiController]
 public class AuthController : ControllerBase
 {
+    private readonly IConfiguration _configuration;
+
+    public AuthController(IConfiguration configuration)
+    {
+        _configuration = configuration;
+    }
+
     [HttpPost]
     public ActionResult Post([FromBody] LoginModel loginModel)
     {
@@ -17,10 +28,37 @@ public class AuthController : ControllerBase
         if (loginModel.EmailAddress == "admin@email.com" && loginModel.Password == "1234")
         {
             // update cookie
+            var authClaims = new List<Claim>
+            {
+                new Claim(ClaimTypes.Name, loginModel.EmailAddress),
+                new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString())
+            };
+
+            var token = CreateToken(authClaims);
+            var refreshToken = "";
+
+
+
+            return Ok();
         }
 
         // return data
         return Ok();
+    }
+
+    private JwtSecurityToken CreateToken(List<Claim> authClaims)
+    {
+        var authSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["JWT:Secret"]));
+        _ = int.TryParse(_configuration["JWT:TokenValidityInMinutes"], out int tokenValidityInMinutes);
+
+        var token = new JwtSecurityToken(
+            issuer: _configuration["JWT:ValidIssuer"],
+            audience: _configuration["JWT:ValidAudience"],
+            expires: DateTime.Now.AddMinutes(tokenValidityInMinutes),
+            claims: authClaims,
+            signingCredentials: new SigningCredentials(authSigningKey, SecurityAlgorithms.HmacSha256)
+        );
+        return token;
     }
 
     public class LoginModel
