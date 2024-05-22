9076b646cd940005510093e4647964fda476c803
Andy Sterkowitz <7583162+andysterks@users.noreply.github.com>
2024-05-21 22:53:38 -0400

    Adds additional info on buy screen

--- src/FantasyStockTrader/ClientApp/src/components/Buy.js
+++ src/FantasyStockTrader/ClientApp/src/components/Buy.js
@@ -1,23 +1,40 @@
-import React from 'react'
-import { useParams } from 'react-router'
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router";
 
 function Buy() {
-  const { symbol } = useParams();    
-    
+  const [buySummary, setBuySummary] = useState(null);
+  const [sharesToBuy, setSharesToBuy] = useState(0)
+
+  const { symbol } = useParams();
+
+  useEffect(() => {
+    axios.get("api/buy/summary", { params: { symbol }})
+        .then(response => setBuySummary(response.data))
+  }, []);
+  
+
   return (
     <>
-        <div>Buy ({symbol})</div>
-        <div>
-            <h3>Current $$$</h3>
-            <p>$100,000.45</p>
-        </div>
-        <div>
-            <h3>Transaction details</h3>
-            <input type="text" placeholder='no. of shares' />
-            <button>Submit</button>
-        </div>
+      <div>Buy ({symbol})</div>
+      <div>
+        <h3>Current $$$</h3>
+        <p>$100,000.45</p>
+      </div>
+      <div>
+        <h3>Transaction details</h3>
+        <input type="text" placeholder="no. of shares" onChange={(e) => { setSharesToBuy(e.target.value) }} value={sharesToBuy} />
+        {buySummary && (
+          <>
+            <p>Current Price: ${buySummary.currentPrice}</p>
+            <p>Max Shares: {buySummary.maxShareAmount}</p>
+            <p>Total cost: ${sharesToBuy * buySummary.currentPrice}</p>
+          </>
+        )}
+        <button>Submit</button>
+      </div>
     </>
-  )
+  );
 }
 
-export default Buy
+export default Buy;
