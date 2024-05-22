8f2969efea77bb160f796a80c78be68616722e2d
Andy Sterkowitz <7583162+andysterks@users.noreply.github.com>
2024-05-21 22:53:38 -0400

    Adds additional info on buy screen

---
+++ src/FantasyStockTrader/Controllers/BuyController.cs
@@ -0,0 +1,34 @@
+﻿using FantasyStockTrader.Core.DatabaseContext;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FantasyStockTrader.Web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BuyController : ControllerBase
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+        private readonly FantasyStockTraderContext _dbContext;
+
+        public BuyController(IHttpClientFactory httpClientFactory, 
+            FantasyStockTraderContext dbContext)
+        {
+            _httpClientFactory = httpClientFactory;
+            _dbContext = dbContext;
+        }
+
+        [HttpGet("summary")]
+        public BuySummary GetBuySummary([FromQuery] string symbol)
+        {
+            var walletBalance = 100000.00;
+
+            var currentPrice = 57.85;
+
+            var maxShareAmount = (int)Math.Floor(walletBalance / currentPrice); 
+
+            return new BuySummary(currentPrice, maxShareAmount);
+        }
+
+        public record BuySummary(double CurrentPrice, int MaxShareAmount);
+    }
+}
