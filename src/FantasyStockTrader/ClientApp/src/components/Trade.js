32fb7cb6af4a90d1ab891dab6f4101ef5cd7760e
Andy Sterkowitz <7583162+andysterks@users.noreply.github.com>
2024-05-21 22:22:23 -0400

    Adds list of companies that redirect to buy page

--- src/FantasyStockTrader/ClientApp/src/components/Trade.js
+++ src/FantasyStockTrader/ClientApp/src/components/Trade.js
@@ -1,12 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
 function Trade() {
+  const [searchQuery, setSearchQuery] = useState("");
+  const [companies, setCompanies] = useState([]);
+
+  const navigate = useNavigate();
+
+  const search = (e) => {
+    e.preventDefault();
+    axios.get('api/company', { params: { query: searchQuery } })
+      .then(response => setCompanies(response.data))
+  }
+
+  const navigateToBuy = (symbol) => {
+    navigate(`/trade/buy/${symbol}`)
+  }
+
   return (
     <div>
       <div>Trade</div>
       <div>Search for symbol =&#62; Buy or sell</div>
       <div>
         <h1>Search</h1>
+        <form onSubmit={search}>
+          <input type="text" placeholder="Enter company symbol" onChange={(e) => setSearchQuery(e.target.value)} value={searchQuery} />
+          <button type="submit">Search</button>
+        </form>
+        <div>
+          <ul>
+            {companies.map(c => <li key={c.symbol} onClick={() => navigateToBuy(c.symbol)}>{c.name} ({c.symbol})</li>)}
+          </ul>
+        </div>
       </div>
       <div>Show available cash somewhere</div>
     </div>
