a0f0e705110f6ca728aa6f5770488ccbf5c035db
Andy Sterkowitz <7583162+andysterks@users.noreply.github.com>
2024-09-08 02:56:16 +0000

    Sep 7, 2024, 10:56 PM

--- src/FantasyStockTrader/ClientApp/src/index.js
+++ src/FantasyStockTrader/ClientApp/src/index.js
@@ -17,7 +17,7 @@ root.render(
   </BrowserRouter>);
 
 // If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
+// unregister() to register()L below. Note this comes with some pitfalls.
 // Learn more about service workers: https://cra.link/PWA
 serviceWorkerRegistration.unregister();
 
