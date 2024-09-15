9faa2ed301fcc1c176492cc52c9d02b23d97af12
Andy Sterkowitz <7583162+andysterks@users.noreply.github.com>
2024-09-15 03:09:49 +0000

    Sep 14, 2024, 11:09 PM

--- src/FantasyStockTrader/ClientApp/src/common/ProtectedRoute.js
+++ src/FantasyStockTrader/ClientApp/src/common/ProtectedRoute.js
@@ -7,6 +7,7 @@ const ProtectedRoute = () => {
   const navigate = useNavigate();
 
   useEffect(() => {
+    console.log("ProtectedRoute - account:", account);
     if (!account) {
       navigate("/login");
     }
