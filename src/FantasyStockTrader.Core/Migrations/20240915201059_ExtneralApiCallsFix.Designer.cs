ffcd615b98f499bef66755573b34ec54f969c943
Andy Sterkowitz <7583162+andysterks@users.noreply.github.com>
2024-09-15 20:11:55 +0000

    Sep 15, 2024, 4:11 PM

--- src/FantasyStockTrader.Core/Migrations/20240915201059_ExtneralApiCallsFix.Designer.cs
+++ src/FantasyStockTrader.Core/Migrations/20240915201059_ExtneralApiCallsFix.Designer.cs
@@ -4,6 +4,7 @@ using FantasyStockTrader.Core.DatabaseContext;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace FantasyStockTrader.Core.Migrations
 {
     [DbContext(typeof(FantasyStockTraderContext))]
-    partial class FantasyStockTraderContextModelSnapshot : ModelSnapshot
+    [Migration("20240915201059_ExtneralApiCallsFix")]
+    partial class ExtneralApiCallsFix
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ src/FantasyStockTrader.Core/Migrations/20240915201059_ExtneralApiCallsFix.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FantasyStockTrader.Core.Migrations
+{
+    /// <inheritdoc />
+    public partial class ExtneralApiCallsFix : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_ExternalApiCalls_AccountId",
+                table: "ExternalApiCalls");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ExternalApiCalls_AccountId",
+                table: "ExternalApiCalls",
+                column: "AccountId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_ExternalApiCalls_AccountId",
+                table: "ExternalApiCalls");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ExternalApiCalls_AccountId",
+                table: "ExternalApiCalls",
+                column: "AccountId",
+                unique: true);
+        }
+    }
+}
