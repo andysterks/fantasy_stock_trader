be5dc3cb16544845531a2f8e3aa4bf0cd45c737d
Andy Sterkowitz <7583162+andysterks@users.noreply.github.com>
2024-12-03 16:02:23 -0500

    WIP

--- src/FantasyStockTrader/Services/HoldingsUpdateService.cs
+++ src/FantasyStockTrader/Services/HoldingsUpdateService.cs
@@ -19,24 +19,40 @@ namespace FantasyStockTrader.Web.Services
 
         public void UpdateWithBuy(Guid accountId, string symbol, int shares, double costBasis) 
         {
-            var holding = _dbContext.Holdings.FirstOrDefault(x => x.Symbol == symbol
-                                                                  && x.Account.Id == accountId);
+            var holding = GetOrCreateHolding(accountId, symbol);
+            UpdateHolding(holding, shares, costBasis);
+            SaveChanges();
+        }
 
-            if (holding == null)
+        private Holding GetOrCreateHolding(Guid accountId, string symbol)
+        {
+            return _dbContext.Holdings.FirstOrDefault(x => x.Symbol == symbol && x.Account.Id == accountId)
+                ?? CreateNewHolding(accountId, symbol);
+        }
+
+        private Holding CreateNewHolding(Guid accountId, string symbol)
+        {
+            var newHolding = new Holding
             {
-                _dbContext.Holdings.Add(new Holding
-                {
-                    AccountId = accountId,
-                    Symbol = symbol,
-                    Shares = shares,
-                    CostBasis = costBasis,
-                    CreatedAt = DateTime.UtcNow
-                });
-                return;
-            }
+                AccountId = accountId,
+                Symbol = symbol,
+                Shares = 0,
+                CostBasis = 0,
+                CreatedAt = DateTime.UtcNow
+            };
+            _dbContext.Holdings.Add(newHolding);
+            return newHolding;
+        }
 
+        private void UpdateHolding(Holding holding, int shares, double costBasis)
+        {
             holding.CostBasis += costBasis;
             holding.Shares += shares;
+        }
+
+        private void SaveChanges()
+        {
+            _dbContext.SaveChanges();
         }
     }
 }
